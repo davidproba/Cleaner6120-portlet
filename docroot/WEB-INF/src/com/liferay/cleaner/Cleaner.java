b586faf863f1b60c153da0dc8297343c4f26bbb8
davidproba <david.proba@liferay.com>
2016-09-09 13:08:51 +0200

    Portlet Logging

--- docroot/WEB-INF/src/com/liferay/cleaner/Cleaner.java
+++ docroot/WEB-INF/src/com/liferay/cleaner/Cleaner.java
@@ -20,51 +20,87 @@ import java.util.List;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Portlet implementation class Cleaner
  */
 public class Cleaner extends MVCPortlet {
+	private static Logger _log = LoggerFactory.getLogger(Cleaner.class);
  
 	public void cleanVersions(ActionRequest aReq, ActionResponse aRes) {
+		/** 
+		 * themeDisplay Objekt notwendig für userId und permissionChecker 
+		 */
 		ThemeDisplay themeDisplay = (ThemeDisplay)aReq.getAttribute(WebKeys.THEME_DISPLAY);
+
+		/** 
+		 * Anzahl zu belassener Fileversionen aus select; 3, 5 oder 10; Standardwert ist 5 
+		 */
 		int  noRest = ParamUtil.getInteger(aReq, "noRest");
 		List<DLFileEntry> files;
-		try {
 
+		try {
+			/** 
+			 * Liste aller Files 
+			 */
 			files = DLFileEntryLocalServiceUtil.getDLFileEntries(0, DLFileEntryLocalServiceUtil.getDLFileEntriesCount());
+
+			/** 
+			 * Permissionabfrage; auch wenn das Portlet nur im Control Panel einsehbar ist und sich in der category.hidden befindet, 
+			 * (siehe liferay-portlet.xml und liferay-display.xml), so darf nur ein Omniadmin oder companyadmin diese Funktion ausdführen
+			 */
 			if(themeDisplay.getPermissionChecker().isOmniadmin()||themeDisplay.getPermissionChecker().isCompanyAdmin()) {
-				
+				int i=0;
+				/**
+				 * Interation durch alle Files
+				 */
 				for(DLFileEntry file:files) {
-					
+					/**
+					 * Abfrage, ob Anzahl der Versionen vom aktuelklen File den voreingestellten Wert übersteigt; da kein Workflow für Dokumente
+					 * hinterlegt sind, haben alle Files und Versions den Status Approved 
+					 */
 					if(file.getFileVersionsCount(WorkflowConstants.STATUS_APPROVED)>noRest) {
-						
+						/**
+						 * Liste aller Versions für dieses File
+						 */
 						List<DLFileVersion> versions = DLFileVersionLocalServiceUtil.getFileVersions(file.getFileEntryId(), 0);
+						/**
+						 * Subliste nach Abzug von noRest Einträgen; versions hat am ersten Index standardmäßig dem aktuellsten Eintrag
+						 */
 						List<DLFileVersion> subVersions = versions.subList(noRest, file.getFileVersionsCount(WorkflowConstants.STATUS_APPROVED));
+						/**
+						 * Iteration durch subVersions, um überflüssige Versions zu löschen;
+						 * DLFileEntryLocalServiceUtill.deleteFileVersions() löscht entsprechende Versions aus DB, Filesystem und zugehörige Expandowerte
+						 */
 						for(DLFileVersion ver : subVersions) {
 							DLFileEntryLocalServiceUtil.deleteFileVersion(themeDisplay.getUserId(), file.getFileEntryId(), ver.getVersion());
 						}
 						
 					}
-					
-					System.out.println(file.getTitle() + " bereiningt.");
+					_log.info(i++ + ". " + file.getTitle() + " bereiningt.");
 				}
-				System.out.println("===========================================================");
-				System.out.println("======================= FERTIG ============================");
-				System.out.println("===========================================================");
+				_log.info("===========================================================");
+				_log.info("======================= FERTIG ============================");
+				_log.info("===========================================================");
 				SessionMessages.add(aReq, "cleaner-success");
 			} else {
 				SessionErrors.add(aReq, "permission-error");
 			}
 			sendRedirect(aReq, aRes);
 		} catch (SystemException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			_log.error("=======================================");
+			_log.error("SystemException im Cleaner");
+			_log.error(e.getStackTrace().toString());
 		} catch (PortalException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			_log.error("=======================================");
+			_log.error("PortalException bei DLFileEntryLocalServiceUtil.deleteFileVersion im Cleaner");
+			_log.error(e.getStackTrace().toString());
+		} catch (IOException e) {
+			_log.error("=======================================");
+			_log.error("IOException bei sendRedirect im Cleaner");
+			_log.error(e.getStackTrace().toString());
 		}
 	}
 
--- docroot/html/cleaner/view.jsp
+++ docroot/html/cleaner/view.jsp
@@ -9,15 +9,17 @@ Files gesamt: <%= DLFileEntryLocalServiceUtil.getDLFileEntriesCount() %><br/>
 Versionen gesamt: <%= DLFileVersionLocalServiceUtil.getDLFileVersionsCount() %><br/>
 
 <%
+/** Liste aller Files **/
 List<DLFileEntry> files = DLFileEntryLocalServiceUtil.getDLFileEntries(0, DLFileEntryLocalServiceUtil.getDLFileEntriesCount());
 long size=0;
+/** Größe aller Fileversions **/
 for(DLFileEntry file:files) {
 	List<DLFileVersion> versions = DLFileVersionLocalServiceUtil.getFileVersions(file.getFileEntryId(), 0);
 	for(DLFileVersion ver : versions) {
 		size+=ver.getSize();
 	}
 }
-
+/** Umrechnung der Fileversiongesamtgröße in KB, MB bzw GB **/
 long sizeP = 0;
 String sizeS = "Bytes";
 if(size>1024) {
@@ -39,9 +41,11 @@ if(size>1024) {
 Gesamtgröße: ca. <%= sizeP %> <%= sizeS %><br/>
 <%
 
+/** aktuelle URL als redirectURL **/
 PortletURL viewURL = renderResponse.createRenderURL();
 viewURL.setWindowState(LiferayWindowState.NORMAL);
 
+/** actionURL zum Antriggern der cleanVersions Methode **/
 PortletURL actionURL = renderResponse.createActionURL();
 actionURL.setParameter(ActionRequest.ACTION_NAME, "cleanVersions");
 actionURL.setParameter("redirectURL", viewURL.toString());
