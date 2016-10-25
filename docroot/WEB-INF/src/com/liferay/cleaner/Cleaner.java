b57d03cf1d5eb3e30f52aeed094f589987933415
davidproba <david.proba@liferay.com>
2016-10-25 15:16:33 +0200

    Details added

--- docroot/WEB-INF/src/com/liferay/cleaner/Cleaner.java
+++ docroot/WEB-INF/src/com/liferay/cleaner/Cleaner.java
@@ -1,17 +1,20 @@
 package com.liferay.cleaner;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.service.LockLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFileEntryConstants;
 import com.liferay.portlet.documentlibrary.model.DLFileVersion;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.service.DLFileVersionLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.store.DLStoreUtil;
+import com.liferay.portlet.expando.service.ExpandoValueLocalServiceUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import java.io.IOException;
@@ -27,6 +30,7 @@ import org.slf4j.LoggerFactory;
  * Portlet implementation class Cleaner
  */
 public class Cleaner extends MVCPortlet {
+
 	private static Logger _log = LoggerFactory.getLogger(Cleaner.class);
  
 	public void cleanVersions(ActionRequest aReq, ActionResponse aRes) {
@@ -61,25 +65,91 @@ public class Cleaner extends MVCPortlet {
 					 * Abfrage, ob Anzahl der Versionen vom aktuelklen File den voreingestellten Wert übersteigt; da kein Workflow für Dokumente
 					 * hinterlegt sind, haben alle Files und Versions den Status Approved 
 					 */
+					_log.info(i++ + " || Starte Bereinigung von " + file.getTitle());
+
 					if(file.getFileVersionsCount(WorkflowConstants.STATUS_APPROVED)>noRest) {
 						/**
 						 * Liste aller Versions für dieses File
 						 */
 						List<DLFileVersion> versions = DLFileVersionLocalServiceUtil.getFileVersions(file.getFileEntryId(), 0);
+						_log.info("Anzahl Versionen gesamt: " + versions.size());
+
 						/**
 						 * Subliste nach Abzug von noRest Einträgen; versions hat am ersten Index standardmäßig dem aktuellsten Eintrag
 						 */
 						List<DLFileVersion> subVersions = versions.subList(noRest, file.getFileVersionsCount(WorkflowConstants.STATUS_APPROVED));
+						_log.info("Anzahl überflüssiger Versionen: " + subVersions.size());
+
+						/**
+						 * FileLock setzen
+						 */
+						_log.info("==> FileLock wird gesetzt");
+						try {
+							if (!DLFileEntryLocalServiceUtil.hasFileEntryLock(themeDisplay.getUserId(), file.getFileEntryId())) {
+								LockLocalServiceUtil.lock(themeDisplay.getUserId(), DLFileEntry.class.getName(), file.getFileEntryId(),
+										null, false, DLFileEntryConstants.LOCK_EXPIRATION_TIME);
+							}
+						} catch (Exception e) {
+							_log.info("Fehler beim Setzen vom FileLock:");
+							_log.info(e.getMessage());
+						}
+
 						/**
 						 * Iteration durch subVersions, um überflüssige Versions zu löschen;
 						 * DLFileEntryLocalServiceUtill.deleteFileVersions() löscht entsprechende Versions aus DB, Filesystem und zugehörige Expandowerte
 						 */
 						for(DLFileVersion ver : subVersions) {
-							DLFileEntryLocalServiceUtil.deleteFileVersion(themeDisplay.getUserId(), file.getFileEntryId(), ver.getVersion());
+/**							DLFileEntryLocalServiceUtil.deleteFileVersion(themeDisplay.getUserId(), file.getFileEntryId(), ver.getVersion()); **/
+							/**
+							 * Lösche Expandos 
+							 */
+							try {
+								_log.info("==> Lösche FileVersion Expandos");
+								ExpandoValueLocalServiceUtil.deleteValues(DLFileVersion.class.getName(), ver.getFileVersionId());
+							} catch (Exception e) {
+								_log.info("Fehler beim Löschen von Expandos:");
+								_log.info(e.getMessage());
+							}
+							
+							/** 
+							 * Bereinige Filesystem
+							 */
+							try {
+								_log.info("==> Lösche Einträge im Filesystem");
+								DLStoreUtil.deleteFile(file.getCompanyId(), file.getDataRepositoryId(), file.getName(), ver.getVersion());
+							} catch (Exception e) {
+								_log.info("Fehler beim Löschen von Expandos:");
+								_log.info(e.getMessage());
+							}
+							
+							/** 
+							 * Lösche Version
+							 */
+							try {
+								_log.info("==> Lösche Version");
+								DLFileVersionLocalServiceUtil.deleteDLFileVersion(ver.getFileVersionId());
+							} catch (Exception e) {
+								_log.info("Fehler beim Löschen von Version:");
+								_log.info(e.getMessage());
+							}
+							
 						}
-						
+
+						/**
+						 * FileLock wieder aufheben
+						 */
+						_log.info("==> FileLock wird aufgehoben");
+						try {
+							LockLocalServiceUtil.unlock(DLFileEntry.class.getName(), file.getFileEntryId());
+						} catch (Exception e) {
+							_log.info("Fehler beim Aufheben vom FileLock:");
+							_log.info(e.getMessage());
+						}
+					} else {
+						_log.info("Zu wenig Versionen, keine Bereinigung notwendig");
 					}
-					_log.info(i++ + ". " + file.getTitle() + " bereiningt.");
+					_log.info(file.getTitle() + " ist fertig.");
+					_log.info("------------------------------");
 				}
 				_log.info("===========================================================");
 				_log.info("======================= FERTIG ============================");
@@ -92,10 +162,6 @@ public class Cleaner extends MVCPortlet {
 		} catch (SystemException e) {
 			_log.error("=======================================");
 			_log.error("SystemException im Cleaner");
-			_log.error(e.getStackTrace().toString());
-		} catch (PortalException e) {
-			_log.error("=======================================");
-			_log.error("PortalException bei DLFileEntryLocalServiceUtil.deleteFileVersion im Cleaner");
 			_log.error(e.getStackTrace().toString());
 		} catch (IOException e) {
 			_log.error("=======================================");
