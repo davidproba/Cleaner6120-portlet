8e5f71910284a8a0ffd8a5d433a8abd8ea19d932
davidproba <david.proba@liferay.com>
2016-12-16 18:54:56 +0100

    Deleting DLFileVersion Expandos mapped to DLFileEntry

--- docroot/WEB-INF/src/com/liferay/cleaner/Cleaner.java
+++ docroot/WEB-INF/src/com/liferay/cleaner/Cleaner.java
@@ -12,9 +12,12 @@ import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLFileEntryConstants;
 import com.liferay.portlet.documentlibrary.model.DLFileVersion;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryMetadataLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.service.DLFileVersionLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.store.DLStoreUtil;
-import com.liferay.portlet.expando.service.ExpandoValueLocalServiceUtil;
+import com.liferay.portlet.expando.model.ExpandoTable;
+import com.liferay.portlet.expando.service.ExpandoRowLocalServiceUtil;
+import com.liferay.portlet.expando.service.ExpandoTableLocalServiceUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import java.io.IOException;
@@ -23,24 +26,25 @@ import java.util.List;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
-
-import java.util.logging.Logger;
-
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Portlet implementation class Cleaner
  */
 public class Cleaner extends MVCPortlet {
 
-	private static Logger _log = Logger.getLogger(Cleaner.class.toString());
- 
+	private static Logger _log = LoggerFactory.getLogger(Cleaner.class.toString());
+	
 	public void cleanVersions(ActionRequest aReq, ActionResponse aRes) {
+
 		/** 
 		 * themeDisplay Objekt notwendig für userId und permissionChecker 
 		 */
 		ThemeDisplay themeDisplay = (ThemeDisplay)aReq.getAttribute(WebKeys.THEME_DISPLAY);
 
+		
+		
 		/** 
 		 * Anzahl zu belassener Fileversionen aus select; 3, 5 oder 10; Standardwert ist 5 
 		 */
@@ -53,35 +57,43 @@ public class Cleaner extends MVCPortlet {
 			 */
 			files = DLFileEntryLocalServiceUtil.getDLFileEntries(0, DLFileEntryLocalServiceUtil.getDLFileEntriesCount());
 
+			
 			/** 
 			 * Permissionabfrage; auch wenn das Portlet nur im Control Panel einsehbar ist und sich in der category.hidden befindet, 
-			 * (siehe liferay-portlet.xml und liferay-display.xml), so darf nur ein Omniadmin oder companyadmin diese Funktion ausdführen
+			 * (siehe liferay-portlet.xml und liferay-display.xml), so darf nur ein Omniadmin oder companyadmin diese Funktion ausfuehren
 			 */
 			if(themeDisplay.getPermissionChecker().isOmniadmin()||themeDisplay.getPermissionChecker().isCompanyAdmin()) {
 				int i=0;
+
+				
 				/**
 				 * Interation durch alle Files
 				 */
 				for(DLFileEntry file:files) {
+
+					
 					/**
-					 * Abfrage, ob Anzahl der Versionen vom aktuelklen File den voreingestellten Wert übersteigt; da kein Workflow für Dokumente
+					 * Abfrage, ob Anzahl der Versionen vom aktuellen File den voreingestellten Wert uebersteigt; da kein Workflow für Dokumente
 					 * hinterlegt sind, haben alle Files und Versions den Status Approved 
 					 */
 					_log.info(i++ + " || Starte Bereinigung von " + file.getTitle());
-
 					if(file.getFileVersionsCount(WorkflowConstants.STATUS_APPROVED)>noRest) {
+
+						
 						/**
 						 * Liste aller Versions für dieses File
 						 */
 						List<DLFileVersion> versions = DLFileVersionLocalServiceUtil.getFileVersions(file.getFileEntryId(), 0);
 						_log.info("Anzahl Versionen gesamt: " + versions.size());
 
+						
 						/**
-						 * Subliste nach Abzug von noRest Einträgen; versions hat am ersten Index standardmäßig dem aktuellsten Eintrag
+						 * Subliste nach Abzug von noRest Eintraegen; versions hat am ersten Index standardmaessig dem aktuellsten Eintrag
 						 */
 						List<DLFileVersion> subVersions = versions.subList(noRest, file.getFileVersionsCount(WorkflowConstants.STATUS_APPROVED));
 						_log.info("Anzahl ueberfluessiger Versionen: " + subVersions.size());
 
+						
 						/**
 						 * FileLock setzen
 						 */
@@ -92,51 +104,73 @@ public class Cleaner extends MVCPortlet {
 										null, false, DLFileEntryConstants.LOCK_EXPIRATION_TIME);
 							}
 						} catch (Exception e) {
-							_log.severe("Fehler beim Setzen vom FileLock:");
-							_log.severe(e.getMessage());
+							_log.error("Fehler beim Setzen vom FileLock:");
+							_log.error(e.getMessage());
 						}
 
+						
 						/**
-						 * Iteration durch subVersions, um überflüssige Versions zu löschen;
-						 * DLFileEntryLocalServiceUtill.deleteFileVersions() löscht entsprechende Versions aus DB, Filesystem und zugehörige Expandowerte
+						 * Iteration durch subVersions, um ueberfluessige Versions zu loeschen;
+						 * DLFileEntryLocalServiceUtill.deleteFileVersions() loescht entsprechende Versions aus DB, Filesystem und zugehoerige Expandowerte
+						 * Zum detaillierten Testen und Loggen werden die Schritte hier einzeln abgearbeitet
 						 */
+						int x = 1;
 						for(DLFileVersion ver : subVersions) {
-/**							DLFileEntryLocalServiceUtil.deleteFileVersion(themeDisplay.getUserId(), file.getFileEntryId(), ver.getVersion()); **/
+
+							
 							/**
-							 * Lösche Expandos 
+							 * Loesche Expandos 
 							 */
 							try {
-								_log.info("==> Loesche FileVersion Expandos");
-								ExpandoValueLocalServiceUtil.deleteValues(DLFileVersion.class.getName(), ver.getFileVersionId());
+								_log.info(x + ".) ==> Loesche FileVersion Expandos");
+								ExpandoTable exTable = ExpandoTableLocalServiceUtil.getDefaultTable(themeDisplay.getCompanyId(), DLFileEntry.class.getName());
+								_log.info(x + ".) ==> ExpandoTable: " + exTable.toString());
+								ExpandoRowLocalServiceUtil.deleteRow(exTable.getTableId(), ver.getFileVersionId());
 							} catch (Exception e) {
-								_log.severe("Fehler beim Loeschen von Expandos:");
-								_log.severe(e.getMessage());
+								_log.error("Fehler beim Loeschen von Expandos:");
+								_log.error(e.getMessage());
 							}
+							
 							
 							/** 
 							 * Bereinige Filesystem
 							 */
 							try {
-								_log.info("==> Loesche Einträge im Filesystem");
+								_log.info(x + ".) ==> Loesche Eintraege im Filesystem");
 								DLStoreUtil.deleteFile(file.getCompanyId(), file.getDataRepositoryId(), file.getName(), ver.getVersion());
 							} catch (Exception e) {
-								_log.severe("Fehler beim Loeschen von Expandos:");
-								_log.severe(e.getMessage());
+								_log.error("Fehler beim Loeschen des Eintrags im Filesystem:");
+								_log.error(e.getMessage());
 							}
+
 							
 							/** 
-							 * Lösche Version
+							 * Loesche Versionsmetadaten
 							 */
 							try {
-								_log.info("==> Loesche Version");
-								DLFileVersionLocalServiceUtil.deleteDLFileVersion(ver.getFileVersionId());
+								_log.info(x + ".) ==> Loesche Versionsmetadaten");
+								DLFileEntryMetadataLocalServiceUtil.deleteFileVersionFileEntryMetadata(ver.getFileVersionId());
 							} catch (Exception e) {
-								_log.severe("Fehler beim Loeschen von Version:");
-								_log.severe(e.getMessage());
+								_log.error("Fehler beim Loeschen von Versionsmetadaten:");
+								_log.error(e.getMessage());
 							}
 							
+							
+							/** 
+							 * Loesche Version
+							 */
+							try {
+								_log.info(x + ".) ==> Loesche Version");
+								DLFileVersionLocalServiceUtil.deleteDLFileVersion(ver.getFileVersionId());
+							} catch (Exception e) {
+								_log.error("Fehler beim Loeschen von Version:");
+								_log.error(e.getMessage());
+							}
+
+							x++;
 						}
 
+						
 						/**
 						 * FileLock wieder aufheben
 						 */
@@ -144,8 +178,8 @@ public class Cleaner extends MVCPortlet {
 						try {
 							LockLocalServiceUtil.unlock(DLFileEntry.class.getName(), file.getFileEntryId());
 						} catch (Exception e) {
-							_log.severe("Fehler beim Aufheben vom FileLock:");
-							_log.severe(e.getMessage());
+							_log.error("Fehler beim Aufheben vom FileLock:");
+							_log.error(e.getMessage());
 						}
 					} else {
 						_log.info("Zu wenig Versionen, keine Bereinigung notwendig");
@@ -162,13 +196,13 @@ public class Cleaner extends MVCPortlet {
 			}
 			sendRedirect(aReq, aRes);
 		} catch (SystemException e) {
-			_log.severe("=======================================");
-			_log.severe("SystemException im Cleaner");
-			_log.severe(e.getStackTrace().toString());
+			_log.error("=======================================");
+			_log.error("SystemException im Cleaner");
+			_log.error(e.getStackTrace().toString());
 		} catch (IOException e) {
-			_log.severe("=======================================");
-			_log.severe("IOException bei sendRedirect im Cleaner");
-			_log.severe(e.getStackTrace().toString());
+			_log.error("=======================================");
+			_log.error("IOException bei sendRedirect im Cleaner");
+			_log.error(e.getStackTrace().toString());
 		}
 	}
 
